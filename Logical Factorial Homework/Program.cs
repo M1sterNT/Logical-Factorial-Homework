a5f7c570e145672aa45d3c55be95084e8384e044
WANDEE SREPENRAK <59050300@kmitl.ac.th>
2017-02-26 15:28:00 +0700

    Added : All Source Code
    Source Code

---
+++ Logical Factorial Homework/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Factorial_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input Fac : ");
+            int fac = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sum  :" + Factorial(fac));
+            Console.ReadLine();
+        }
+        static int Factorial(int fac) {
+            if (fac == 1 || fac == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return fac * Factorial(fac - 1);
+            }
+            
+        }
+    }
+}
